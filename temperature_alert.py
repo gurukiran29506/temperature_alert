adfe267de98579e835cce4d5c467115800a6040f
gurukiran29506 <gurukiranhippargi@gmail.com>
2025-11-27 13:08:35 +0530

    Add command-line argument for temperature input

---
+++ temperature_alert.py
@@ -0,0 +1,13 @@
+import sys
+if len(sys.argv) == 2:
+  script_name = sys.argv[0]
+  temp = sys.argv[1]
+else:
+  temp = 25
+
+if temp >40:
+  print("hot")
+elif temp>20:
+  print("normal")
+else:
+  print("cold")
